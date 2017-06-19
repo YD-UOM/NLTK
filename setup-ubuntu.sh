87ff70f77fd67bd60ac91ac66bbfad4e387359be
Kim Doyle <kcdoy1@gmail.com>
2017-06-19 16:06:38 +1000

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,6 +6,8 @@ echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> $HOME/.bashrc
 export PATH="$HOME/miniconda/bin:$PATH"
 . ~/.bashrc
 conda install jupyter nltk
+wget https://raw.githubusercontent.com/doylek1/NLTK/master/nltk-session-1-beginners-empty.ipynb -O $HOME/nltk-session-1-beginners-empty.ipynb
+wget https://raw.githubusercontent.com/doylek1/NLTK/master/nltk-session-2-beginners-empty.ipynb -O $HOME/nltk-session-2-beginners-empty.ipynb
 
 echo
 echo 'SETUP COMPLETE.  ENJOY RESPLATTING!'
