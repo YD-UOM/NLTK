8a0cf016d150381be32ff601292069a335295b55
Kim Doyle <kcdoy1@gmail.com>
2017-06-19 15:23:38 +1000

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --output /tmp/Miniconda3-latest-Linux-x86_64.sh
-bash /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh --output /tmp/Miniconda3-latest-MacOSX-x86_64.sh
+bash /tmp/Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/miniconda
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> $HOME/.bash_profile
 export PATH="$HOME/miniconda/bin:$PATH"
 . ~/.bash_profile
