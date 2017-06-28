bd753b93550ca62306ac9ce769ee2c8265656dfc
rt6 <rt6@users.noreply.github.com>
2017-06-28 11:47:38 +1000

    install matplotlib

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,7 +5,7 @@ bash /tmp/Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/miniconda
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> $HOME/.bash_profile
 export PATH="$HOME/miniconda/bin:$PATH"
 . ~/.bash_profile
-conda install jupyter nltk numpy
+conda install jupyter nltk numpy matplotlib
 curl https://raw.githubusercontent.com/doylek1/NLTK/master/nltk-session-1-beginners-empty.ipynb --output $HOME/nltk-session-1-beginners-empty.ipynb
 
 curl https://raw.githubusercontent.com/doylek1/NLTK/master/nltk-session-2-beginners-empty.ipynb --output $HOME/nltk-session-2-beginners-empty.ipynb
